3c35140edc719c8c1a649edf9e57ad75cf4f033c
sswiger <sswiger@forhims.com>
2023-05-10 17:27:30 -0600

    added html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,8 @@
 const path = require("path");
+const notes = require('express').Router();
 
-module.exports = function(app) {
-    app.get("/", function(req, res){
-        res.sendFile(path.join(__dirname, "../"))
-    })
+notes.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
-
-}
+module.exports = notes;
