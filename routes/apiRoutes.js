5e9dc95d3739f873c4a628a2403bd9f75c50dd71
sswiger <sswiger@forhims.com>
2023-05-10 18:01:56 -0600

    added utils.js and updated apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,4 @@
 const router = require('express').Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { fileRead, fileWrite, readAndAppend }= require('../utils.js')
---
+++ utils.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const util = require('util');
+
+const fileRead = util.promisify(fs.readFile);
+
+//Writes data to a given destination 
+const fileWrite = (destination, content) => {
+    fs.writeFileSync(destination, JSON.stringify(content, null, 4), (err) => 
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+};
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            fileWrite(file, parsedData);
+        }
+    });
+};
+
+module.exports = { fileRead, fileWrite, readAndAppend };
