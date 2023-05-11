28818faa73a7ea47b886699a1a4187fb7eec804b
sswiger <sswiger@forhims.com>
2023-05-10 18:16:52 -0600

    updated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,28 @@
 const router = require('express').Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const { fileRead, fileWrite, readAndAppend }= require('../utils.js')
+const { fileRead, fileWrite, readAndAppend }= require('../utils.js');
+
+const database = require('../db/db.json');
+
+router.get('/notes', (req, res) => {
+    fileRead(database)
+    .then((data) => {
+    res.json(JSON.parse(data));
+});
+});
+
+router.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            title, 
+            text,
+            id: uuidv4()
+        };
+        readAndAppend(newNote, database);
+        res.json('Note was added successfully');
+    } else {
+        res.errored('Note was not added');
+    }
+});
